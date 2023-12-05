fde7669a3f84d356c35c09bb14e7dab0fce663bb
Mutasem_Ahmad <mutasemdaoudahmad@gmail.com>
2023-12-05 10:52:32 +0300

    update some changes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const feeds = [
-    // { name: 'aljazeera', url: 'https://www.aljazeera.net/aljazeerarss/a7c186be-1baa-4bd4-9d80-a84db769f779/73d0e1b4-532f-45ef-b135-bfdff8b8cab9' },
+    { name: 'aljazeera', url: 'https://www.aljazeera.net/aljazeerarss/a7c186be-1baa-4bd4-9d80-a84db769f779/73d0e1b4-532f-45ef-b135-bfdff8b8cab9' },
     { name: 'alwakeel', url: 'https://www.alwakeelnews.com/rss' }
 ];
