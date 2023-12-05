cea7a201295d5e5b32b75adb7bf795905e182f6c
Mutasem_Ahmad <mutasemdaoudahmad@gmail.com>
2023-12-05 15:58:26 +0300

    update config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export const feeds = [
     { name: 'Aljazeera', url: 'https://www.aljazeera.net/aljazeerarss/a7c186be-1baa-4bd4-9d80-a84db769f779/73d0e1b4-532f-45ef-b135-bfdff8b8cab9' },
-    { name: 'Alwakeel', url: 'https://www.alwakeelnews.com/rss' },
-    { name: 'CNN', url: 'http://rss.cnn.com/rss/edition_football.rss' }
+    { name: 'Alwakeel', url: 'https://www.alwakeelnews.com/rss' }
 ];
